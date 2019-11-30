d588f5d743542c07b1037e0be4d53770175deaec
lethithuhuong99 <huong.le170205@vnuk.edu.vn>
2019-11-30 10:14:43 +0700

    finish user profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_29_091756) do
+ActiveRecord::Schema.define(version: 2019_11_30_014724) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -39,6 +39,8 @@ ActiveRecord::Schema.define(version: 2019_11_29_091756) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
